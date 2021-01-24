a4288731a4e4ff57a64a062df72282021ffb3dd4
Mandeep Kaur <m.kaur.nz@gmail.com>
2021-01-24 15:19:10 +1300

    Implement  Excercise

--- VLCTest2/Util/UtilClass.cs
+++ VLCTest2/Util/UtilClass.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace VLCTest2.Util
 {
     public class UtilClass: Constants
     {
+        public void SwitchToCurrentWindow()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1.5));
+            var allWindowHandles2 = _driver.WindowHandles;
+            _driver.SwitchTo().Window(allWindowHandles2[0]);
+        }
+
+        public void SwitchToPreviousWindow()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1.5));
+            var allWindowHandles2 = _driver.WindowHandles;
+            _driver.SwitchTo().Window(allWindowHandles2[1]);
+        }
     }
 }
